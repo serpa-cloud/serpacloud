41af91a4d60ab52baeb975a8514b4aaf65ad89c7
Emilia González <emilia@serpa.cloud>
2024-10-27 11:48:41 -0600

    fix(docker): render from nginx

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,39 @@
-FROM node:lts
+# Etapa 1: Compilar la aplicación React
+FROM node:20-alpine AS build
 
-WORKDIR /
-
-COPY package*.json ./
-
-COPY yarn.lock ./
-
-RUN yarn
-
-ENV PORT=80
-
-ENV NODE_ENV=production
+# Crear directorio de trabajo
+WORKDIR /app
 
 ENV ASSET_PATH="https://d1icgfgxibs78l.cloudfront.net/"
 
 ENV BASE_DOMAIN="serpa.cloud"
 
+# Copiar package.json y package-lock.json
+COPY package*.json ./
+
+# Instalar dependencias
+RUN yarn -only=prod
+
+# Copiar el resto del código de la aplicación
 COPY . .
 
-RUN yarn build && yarn build:webpack
+# Compilar la aplicación
+RUN yarn build
 
+# Etapa 2: Configurar Nginx para servir la aplicación
+FROM nginx:stable-alpine
+
+# Elimina la configuración por defecto de Nginx
+RUN rm -rf /usr/share/nginx/html/*
+
+# Copia los archivos generados en el paso anterior a la carpeta de Nginx
+COPY --from=build /app/build /usr/share/nginx/html
+
+# Copiar archivo de configuración personalizado de Nginx si lo tienes
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+# Exponer el puerto 80 para el contenedor
 EXPOSE 80
 
-CMD [ "npm", "start" ]
+# Comando por defecto para iniciar Nginx
+CMD ["nginx", "-g", "daemon off;"]
--- config/webpack.server.js
+++ config/webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const StylexPlugin = require('@serpa-cloud/stylex-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 
 const paths = require('./paths');
 
@@ -22,6 +24,75 @@ const hasJsxRuntime = (() => {
 })();
 
 const version = require('../package.json').version;
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+const getStyleLoaders = (cssOptions, preProcessor) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      // css is located in `static/css`, use '../../' to locate index.html folder
+      // in production `paths.publicUrlOrPath` can be a relative path
+      options: {},
+    },
+    {
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    },
+    {
+      // Options for PostCSS as we reference these options twice
+      // Adds vendor prefixing based on your specified browser support in
+      // package.json
+      loader: require.resolve('postcss-loader'),
+      options: {
+        postcssOptions: {
+          // Necessary for external CSS imports to work
+          // https://github.com/facebook/create-react-app/issues/2677
+          ident: 'postcss',
+          config: false,
+          plugins: [
+            'postcss-flexbugs-fixes',
+            [
+              'postcss-preset-env',
+              {
+                autoprefixer: {
+                  flexbox: 'no-2009',
+                },
+                stage: 3,
+              },
+            ],
+            // Adds PostCSS Normalize as the reset css with default options,
+            // so that it honors browserslist config in package.json
+            // which in turn let's users customize the target behavior as per their needs.
+            'postcss-normalize',
+          ],
+        },
+        sourceMap: true,
+      },
+    },
+  ].filter(Boolean);
+  if (preProcessor) {
+    loaders.push(
+      {
+        loader: require.resolve('resolve-url-loader'),
+        options: {
+          sourceMap: true,
+          root: paths.appSrc,
+        },
+      },
+      {
+        loader: require.resolve(preProcessor),
+        options: {
+          sourceMap: true,
+        },
+      },
+    );
+  }
+  return loaders;
+};
 
 module.exports = {
   entry: './src/server.js',
@@ -78,7 +149,7 @@ module.exports = {
           {
             loader: require.resolve('file-loader'),
             options: {
-              name: `static/media/[name].${version}.[ext]`,
+              name: `static/media/[name].${version}.[hash].[ext]`,
             },
           },
         ],
@@ -86,6 +157,7 @@ module.exports = {
           and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
         },
       },
+
       {
         test: /\.(js|mjs|jsx|ts|tsx)$/,
         include: paths.appSrc,
@@ -114,8 +186,77 @@ module.exports = {
           },
         ],
       },
+
+      {
+        test: cssRegex,
+        exclude: cssModuleRegex,
+        use: getStyleLoaders({
+          importLoaders: 1,
+          sourceMap: true,
+          modules: {
+            mode: 'icss',
+          },
+        }),
+        // Don't consider CSS imports dead code even if the
+        // containing package claims to have no side effects.
+        // Remove this when webpack adds a warning or an error for this.
+        // See https://github.com/webpack/webpack/issues/6571
+        sideEffects: true,
+      },
+
+      {
+        test: sassRegex,
+        exclude: sassModuleRegex,
+        use: getStyleLoaders(
+          {
+            importLoaders: 3,
+            sourceMap: true,
+            modules: {
+              mode: 'icss',
+            },
+          },
+          'sass-loader',
+        ),
+        // Don't consider CSS imports dead code even if the
+        // containing package claims to have no side effects.
+        // Remove this when webpack adds a warning or an error for this.
+        // See https://github.com/webpack/webpack/issues/6571
+        sideEffects: true,
+      },
+      // Adds support for CSS Modules, but using SASS
+      // using the extension .module.scss or .module.sass
+      {
+        test: sassModuleRegex,
+        use: getStyleLoaders(
+          {
+            importLoaders: 3,
+            sourceMap: true,
+            modules: {
+              mode: 'local',
+              getLocalIdent: getCSSModuleLocalIdent,
+            },
+          },
+          'sass-loader',
+        ),
+      },
+      {
+        // Exclude `js` files to keep "css" loader working as it injects
+        // its runtime that would otherwise be processed through "file" loader.
+        // Also exclude `html` and `json` extensions so they get processed
+        // by webpacks internal loaders.
+        exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+        type: 'asset/resource',
+      },
     ],
   },
 
-  plugins: [new StylexPlugin()],
+  plugins: [
+    new StylexPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: `static/css/[name].${version}.css`,
+      chunkFilename: `static/css/[name].${version}.chunk.css`,
+    }),
+  ],
 };
